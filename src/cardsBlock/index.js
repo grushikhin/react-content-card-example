d8d4dead225c7d9a22608b984d9c732e267aa237
grushikhin <grushikhin@gmail.com>
2015-12-23 02:03:00 +0300

    update from demo

--- src/cardsBlock/index.js
+++ src/cardsBlock/index.js
@@ -1,18 +1,22 @@
+var CardsBlockControls = require('../cardsBlockControls/index');
+
 module.exports = React.createClass({
     render: function() {
         return (
             <div className="cardsBlock">
                 <div className="cardsBlock-content">
                     <div className="cardsBlock-title">
-                        <span className="glyphicon glyphicon-film"></span>&nbsp;{this.props.title}
+                        <span className="glyphicon glyphicon-facetime-video"></span>&nbsp;{this.props.title}
                     </div>
-                    <p className="cardsBlock-description">
-                        {this.props.description1}
-                    </p>
+                    <div className="cardsBlock-description">
+                        {this.props.description}
+                    </div>
                 </div>
-                <div className="cardsBlock-background" style={ {backgroundImage: 'url(' + this.props.image + ')' }}></div>
+                <div className="cardsBlock-background" style={{backgroundImage: 'url(' + this.props.image + ')' }}></div>
                 <div className="cardsBlock-overlay"></div>
-
+                <div className="cardsBlock-tooltip">
+                    <CardsBlockControls id={this.props.id}></CardsBlockControls>
+                </div>
             </div>
         );
     }
--- src/header/index.js
+++ src/header/index.js
@@ -2,13 +2,11 @@ module.exports = React.createClass({
     render: function() {
         return <div className="header">
             <div className="container">
-                <div className="row">
-                    <div className="header-title">
-                        React content cards example
-                    </div>
-                    <div className="header-link">
-                        <a href="#">Go to source</a>
-                    </div>
+                <div className="header-title">
+                    React content cards example
+                </div>
+                <div className="header-link">
+                    <a href={this.props.url}>View on github</a>
                 </div>
             </div>
         </div>
